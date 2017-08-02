019e8f5c9c19eb3e3b2f0878208210eb645d52a8
Jason Crawford <daxapps777@gmail.com>
2017-08-02 11:33:25 -0500

    Set up CORS

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
- const express = require('express');
- const app = express();
+const express = require('express');
+const app = express();
 
- const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
- app.get('/api/*', (req, res) => {
-   res.json({ok: true});
- });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
- app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.get('/api/*', (req, res) => {
+res.json({ok: true});
+});
 
- module.exports = {app};
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = {app};
