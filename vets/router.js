4b251ce2f7cb40858afd07bc73694bb7b2fb8f9b
Jason Crawford <daxapps777@gmail.com>
2017-08-24 07:15:59 -0500

    add Servie link and services appears when vet is clicked

--- vets/router.js
+++ vets/router.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 
 const { Vet } = require("./models");
-const { Service }  = require("../services/models");
+const { Service } = require("../services/models");
 
 const router = express.Router();
 
@@ -18,14 +18,29 @@ router.post("/vetlist", jsonParser, (req, res) => {
   console.log("DATAID: ", req.body.googleDataId);
   var newVet = {
     googleDataId: googleDataId,
-    vetName: vetName,
-    // servicesRef: servicesRef
+    vetName: vetName
+    // servicesRef: servicesRef will reset to []
   };
 
-  Vet.findOneAndUpdate({ googleDataId: newVet.googleDataId }, newVet, {
-    upsert: true
-  })
-    .then(vet => res.status(201).json(vet))
+  Vet.findOne({ googleDataId: googleDataId })
+    .exec()
+    .then(vet => {
+      console.log("VET: ", vet);
+      if (vet) {
+        Vet.findByIdAndUpdate(vet._id, { $set: newVet }, { new: true })
+          .exec()
+          .then(vet => {
+            console.log("RESSTATUS: ", vet);
+            return res.status(201).json(vet);
+          });
+      } else {
+        Vet.create(newVet).then(vet => vet);
+      }
+    })
+    // .then(vet => {
+    //   console.log("RESSTATUS: ", vet);
+    //   return res.status(201).json(vet);
+    // })
     // vet => res.status(201).redirect('/vets/vet/'+ vet._id))
     .catch(err => {
       console.error(err);
@@ -33,39 +48,37 @@ router.post("/vetlist", jsonParser, (req, res) => {
     });
 });
 
-router.post('/:id/services', jsonParser, (req, res) => {
-  console.log('SERVICETEST', req, req.body);
-  Service
-    .create({
-      _creator:req.params.id, 
-      service:req.body.service, 
-      price:req.body.price
-    })
-    .then(service => 
-      Vet.findOneAndUpdate({_id:req.params.id}, {$push: {"servicesRef": {_id: service._id}}}, {safe: true, upsert: true})
-      
+router.post("/:id/services", jsonParser, (req, res) => {
+  console.log("SERVICETEST", req, req.body);
+  Service.create({
+    _creator: req.params.id,
+    service: req.body.service,
+    price: req.body.price
+  })
+    .then(service =>
+      Vet.findOneAndUpdate(
+        { _id: req.params.id },
+        { $push: { servicesRef: { _id: service._id } } },
+        { safe: true, upsert: true }
       )
+    )
     .then(data => res.status(201).json(data.apiRepr()))
     .catch(err => {
       console.error(err);
-      res.status(500).json({message: 'Internal server error'});
-      });
-})
+      res.status(500).json({ message: "Internal server error" });
+    });
+});
 
-router.get('/vetlist/:id', (req, res) => {
-
-  Vet
-    .findById(req.params.id)
-    .populate("servicesRef")
-    .exec(function(err, vet){
-      if(err){
-        console.log(err)
-      }
-      console.log('VET: ', vet)
-      res.json(vet)
-    })
-    // .then(vet => res.json(vet.apiRepr()))
-    // .catch(err => res.status(500).json({message: 'Internal server error' + err}));
+router.get("/vetlist/:id", (req, res) => {
+  Vet.findById(req.params.id).populate("servicesRef").exec(function(err, vet) {
+    if (err) {
+      console.log(err);
+    }
+    console.log("VET: ", vet);
+    res.json(vet);
+  });
+  // .then(vet => res.json(vet.apiRepr()))
+  // .catch(err => res.status(500).json({message: 'Internal server error' + err}));
 });
 
 module.exports = { router };
