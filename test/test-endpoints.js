2b8b85b5dc40a9d14f03a5cff2b14554f66e0bbe
Jason Crawford <daxapps777@gmail.com>
2017-09-10 20:35:55 -0500

    Test POST vet listing

---
+++ test/test-endpoints.js
@@ -0,0 +1,54 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {TEST_DATABASE_URL} = require('../config');
+const {app, runServer, closeServer} = require('../server');
+const {Vet} = require('../vets');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Vet endpoints', () => {
+	// const googleDataId = '1234453668';
+ //  const vetName = 'Petsmart';
+
+	before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  // beforeEach(function() {
+  //   return Vet.create({
+  //       googleDataId,
+  //       vetName
+  //     })
+  //   );
+  // });
+
+  afterEach(function() {
+    return Vet.remove({});
+  });
+
+  describe('POST endpoint', () => {
+  	it('should post vet listing', () => {
+	  	return chai.request(app)
+	  	.post('/api/vets/vetlist')
+	  	.send({googleDataId: '1254234623', vetName: 'Petsmart'})
+	  	.then(res => {
+          expect(res).to.have.status(302);
+				})
+	  	.catch(err => {
+				if (err) console.log('Something went wrong: ' + err)
+			});
+	  });	
+  });
+	  
+
+});
+
+
+
